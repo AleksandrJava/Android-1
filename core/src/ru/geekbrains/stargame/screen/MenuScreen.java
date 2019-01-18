16bcdccebbee903811505e906b666330e7052c6f
AleksandrJava <44460921+AleksandrJava@users.noreply.github.com>
2019-01-18 22:44:41 +0400

    добавлена работающая реализация игры

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -13,8 +13,10 @@ public class MenuScreen extends Base2DScreen {
     Texture img;
     Texture background;
 
-    Vector2 pos;
-    Vector2 v;
+    Vector2 pos;//вектор положения
+    Vector2 dest;//вектор конечной точки
+    Vector2 speed;//вектор скорости и направления
+    float line;
 
     @Override
     public void show() {
@@ -23,8 +25,10 @@ public class MenuScreen extends Base2DScreen {
         background = new Texture("new.jpg");
         img = new Texture("bee2.png");
         pos = new Vector2(0, 0);
-        v = new Vector2(1,1);
+        dest = new Vector2(0,0);
+        speed = new Vector2(0,0);
     }
+
 
     @Override
     public void render(float delta) {
@@ -35,9 +39,15 @@ public class MenuScreen extends Base2DScreen {
         batch.draw(background, 0, 0);
         batch.draw(img, pos.x, pos.y);
         batch.end();
-//        if (Gdx.graphics.getWidth() - 256 > pos.x && Gdx.graphics.getHeight() - 256 > pos.y) {
-//            pos.add(v);
-//        }
+
+        if(Gdx.graphics.getWidth() - 64 > pos.x && Gdx.graphics.getHeight() - 64 > pos.y && pos.x >= 0 && pos.y >= 0) {
+            pos.add(speed);
+        }
+        if (Gdx.graphics.getWidth() - 64 > pos.x && Gdx.graphics.getHeight() - 64 > pos.y && pos.x > 0 && pos.y > 0) {
+
+            pos.add(speed);
+            System.out.println("Текущие координаты " + pos.x + " " + pos.y);
+        }
     }
 
     @Override
@@ -55,6 +65,13 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
+
+        dest.set(screenX-32-pos.x, (Gdx.graphics.getHeight() - screenY-32-pos.y));
+        speed.set(dest).nor().scl(1.5f);
+        pos.add(speed);
+        line = dest.len();
+
         return super.touchDown(screenX, screenY, pointer, button);
     }
+
 }
